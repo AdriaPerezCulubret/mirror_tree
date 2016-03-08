f85c88e2e13a25cfa0f221979a2408e17bd090ea
joanmarticarreras <joan.marti.carreras@gmail.com>
2016-03-08 17:26:58 +0100

    is now a function

--- aln_test.py
+++ aln_test.py
@@ -13,28 +13,29 @@ from Bio.Align.Applications import TCoffeeCommandline     as tcoffee
 
 #-----------------------------------------------------------------------
 
-aln = AlignIO.read(open('tmp/1.fa.aln'), 'clustal')
-calculator = DistanceCalculator('blosum62')
-dist_matrix = calculator.get_distance(aln)
-
-
-i=0
-j=0
-da_list = list()
-for row in dist_matrix:
-    print ('New Row!')
+def get_dist_matrix (file):
+    aln = AlignIO.read(open('tmp/'+file), 'clustal')
+    calculator = DistanceCalculator('blosum62')
+    dist_matrix = calculator.get_distance(aln)
+    i=0
     j=0
-    for column in row:
-        if i<j:          # with this, you take out the 0's so n = (N²-N)/2
-            print (dist_matrix[i,j])
-            da_list.append(dist_matrix[i,j])
-            
-        j+=1
-    i+=1
-print (da_list)
-print(len(da_list))
+    da_list = list()
+    for row in dist_matrix:
+        print ('New Row!')
+        j=0
+        for column in row:
+            if i<j:          # with this, you take out the 0's so n = (N²-N)/2
+                print (dist_matrix[i,j])
+                da_list.append(dist_matrix[i,j])
+            j+=1
+        i+=1
+    return (da_list)
+
+
+da_list = list(get_dist_matrix('1.fa.aln'))
+
 result = np.corrcoef(da_list,da_list)[1,0]
-print("Result: "+str(result))
+print("Correlation coeficient: "+str(result))
 
 
 
